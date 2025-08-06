a345b2fa57b8c0b62b0af9f094a896a015a1aef1
Matt Huesman <45442854+matt-huesman@users.noreply.github.com>
2025-08-05 20:20:53 -0500

    Fixed Adapter Import

--- svelte.config.js
+++ svelte.config.js
@@ -1,11 +1,11 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
-import adapter from '@sveltejs/adapter-static';
 
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { adapter: adapter({
+	kit: { 
+		adapter: adapter({
           pages: 'build', // Output directory for static pages
           assets: 'build', // Output directory for static assets
           fallback: null, // Set to null for 404 handling if needed
