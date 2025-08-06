c6343c151eaeb7e7efca9238f13fa1ec261dafb7
Matt Huesman <45442854+matt-huesman@users.noreply.github.com>
2025-08-05 20:55:01 -0500

    Fixed config

--- svelte.config.js
+++ svelte.config.js
@@ -2,6 +2,8 @@ import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 import tailwindcss from '@tailwindcss/vite';
 
+const dev = process.env.NODE_ENV === 'development'; // ✅ ADD THIS
+
 const config = {
 	preprocess: vitePreprocess(),
 	kit: { 
