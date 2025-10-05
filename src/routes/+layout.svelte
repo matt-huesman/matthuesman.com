88f1a3a2eb2a8649fd8624e4c23b022ec463a45a
Matt Huesman <45442854+matt-huesman@users.noreply.github.com>
2025-10-04 20:48:40 -0500

    Changed Paths to absolute

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,14 +2,15 @@
 	import '../app.css';
 	import Header from './Header.svelte';
 	import HeaderItem from './HeaderItem.svelte';
+	// import { base } from '$app/paths'
 
 	let { children } = $props();
 </script>
 
 <Header>
-	<HeaderItem href="./">About Me</HeaderItem>
-	<HeaderItem href="./me">Projects</HeaderItem>
-	<HeaderItem href="./">Contact</HeaderItem>
+	<HeaderItem href="/">About Me</HeaderItem>
+	<HeaderItem href="/me">Projects</HeaderItem>
+	<HeaderItem href="/contact">Contact</HeaderItem>
 	<i class="fa-solid fa-circle-half-stroke"></i>
 </Header>
 
