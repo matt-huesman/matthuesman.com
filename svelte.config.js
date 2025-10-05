5b754263023874b31767dabd15975ca4f1ff9a32
Matt Huesman <45442854+matt-huesman@users.noreply.github.com>
2025-10-04 20:26:25 -0500

    Updated Svelte Config

--- svelte.config.js
+++ svelte.config.js
@@ -1,25 +1,21 @@
 import adapter from '@sveltejs/adapter-static';
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
-import tailwindcss from '@tailwindcss/vite';
 
 const base = process.env.BASE_PATH ?? '';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
 	preprocess: vitePreprocess(),
-	kit: { 
+	kit: {
 		adapter: adapter({
 			pages: 'build',
 			assets: 'build',
 			fallback: undefined,
 			precompress: false,
 			strict: true
-        }),
+		}),
 		paths: {
 			base: base
-		},
-		vite: {
-			plugins: [tailwindcss()]
 		}
 	}
 };
